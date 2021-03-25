494b2b3728acae397f40d0d36777bd4cd901537d
Mikhail Ionin <mikhail.ionin@gmail.com>
2021-03-25 12:24:37 +0700

    1 commit. Local for beginning

---
+++ HW 2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.1.1
+//
+//  Created by Ионин Михаил Викторович on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
